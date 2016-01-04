09b8039b7edd9baf1a7f01cbb55d25e7ebabe3db
Espen Gudmundsen <espen.gudmundsen@accenture.com>
2016-01-04 19:53:43 +0100

    Added Parse Login UI integration. Todo: User profile + back-button touched -> the user should log-out..

--- LearningBasics2/app/src/main/java/no/bacheloroppgaven/android/learningbasics2/MainActivity.java
+++ LearningBasics2/app/src/main/java/no/bacheloroppgaven/android/learningbasics2/MainActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ui.ParseLoginBuilder;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +25,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
         startActivityForResult(builder.build(), 0);
-
-        Button btn_login = (Button) findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_login:
-                Intent i = new Intent(getApplicationContext(), UserProfileActivity.class);
-                startActivity(i);
-                break;
-            default:
-                Toast.makeText(MainActivity.this, R.string.error_general, Toast.LENGTH_SHORT).show();
-        }
     }
 }
