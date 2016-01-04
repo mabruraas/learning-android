a0ad9c1c886f7791bc0c09e033f683299b811380
mabruraas <mabruraas@gmail.com>
2016-01-04 19:34:12 +0100

    LB-2: Added Parse Login functionality.

--- LearningBasics2/app/src/main/java/no/bacheloroppgaven/android/learningbasics2/MainActivity.java
+++ LearningBasics2/app/src/main/java/no/bacheloroppgaven/android/learningbasics2/MainActivity.java
@@ -1,13 +1,43 @@
 package no.bacheloroppgaven.android.learningbasics2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.parse.Parse;
+import com.parse.ui.ParseLoginBuilder;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.initialize(this);
+
+        initView();
+    }
+
+    private void initView() {
+        ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
+        startActivityForResult(builder.build(), 0);
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                Intent i = new Intent(getApplicationContext(), UserProfileActivity.class);
+                startActivity(i);
+                break;
+            default:
+                Toast.makeText(MainActivity.this, R.string.error_general, Toast.LENGTH_SHORT).show();
+        }
     }
 }
