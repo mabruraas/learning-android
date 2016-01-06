6624af758a293eeaf6902e96bd00e35a35f7ff79
mabruraas <mabruraas@gmail.com>
2016-01-06 18:59:24 +0100

    CC-0: created project ColorClicker.

---
+++ ColorClicker/app/src/main/java/no/bacheloroppgaven/android/colorclicker/MainActivity.java
@@ -0,0 +1,13 @@
+package no.bacheloroppgaven.android.colorclicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
