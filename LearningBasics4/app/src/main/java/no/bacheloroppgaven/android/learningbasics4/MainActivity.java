fb2d2d82d8fec843ac2f8114be4534f2660bec1d
mabruraas <mabruraas@gmail.com>
2016-01-06 16:22:11 +0100

    LB-4: Added multiple log statements for debugging app.

--- LearningBasics4/app/src/main/java/no/bacheloroppgaven/android/learningbasics4/MainActivity.java
+++ LearningBasics4/app/src/main/java/no/bacheloroppgaven/android/learningbasics4/MainActivity.java
@@ -15,6 +15,7 @@ import com.parse.Parse;
 import no.bacheloroppgaven.android.learningbasics4.services.ActionExecutorService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView vActiveActions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +24,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Parse.initialize(this);
 
         initButtons();
+        initTextFields();
     }
 
     private void initButtons() {
+        Log.d("Init", "Initializing buttons");
         Button btn = (Button) findViewById(R.id.btn_trigger);
         btn.setOnClickListener(this);
+    }
+
+    private void initTextFields() {
+        vActiveActions = (TextView) findViewById(R.id.txt_action_list);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_trigger:
+                Log.d("OnClick", "Init IntentService, and trigger");
                 startService(new Intent(getApplicationContext(), ActionExecutorService.class));
                 break;
             default:
-                Log.d("btn", "Could not find button");
+                Log.d("OnClick", "Could not find button");
         }
     }
 
@@ -45,11 +53,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            TextView vActiveActions = (TextView) findViewById(R.id.txt_action_list);
+            Log.d("ActionBroadcaster", "BroadcastReceiver triggered");
+
+            Log.d("ActionBroadcaster", "Getting active actions from parameter intent sent from service");
             String[] activeActions = (String[]) intent.getExtras().get("activeActions");
 
-            if (vActiveActions != null && activeActions != null) {
+            Log.d("ActionBroadcaster", "ActiveActions.length: " + activeActions.length);
+            Log.d("ActionBroadcaster", "View component text: " + vActiveActions.getText());
+            if (vActiveActions != null) {
                 for (String activeAction : activeActions) {
+                    Log.d("ActionBroadcaster", "Adding "+ activeAction + " as action to view component");
                     vActiveActions.setText(vActiveActions.getText() + "\n" + activeAction);
                 }
             }
--- LearningBasics4/app/src/main/java/no/bacheloroppgaven/android/learningbasics4/services/ActionExecutorService.java
+++ LearningBasics4/app/src/main/java/no/bacheloroppgaven/android/learningbasics4/services/ActionExecutorService.java
@@ -2,6 +2,7 @@ package no.bacheloroppgaven.android.learningbasics4.services;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -24,26 +25,34 @@ public class ActionExecutorService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         // Bundle extras = intent.getExtras(); // For sending service parameters
         List<String> actions = getActiveActions();
-        Intent broadcastedIntent = new Intent();
+        Intent broadcasterIntent = new Intent();
 
-        broadcastedIntent.putExtra("activeActions", actions.toArray());
+        Log.d("ActionExecutorIntentService", "Putting active actions on broadcaster intent");
+        broadcasterIntent.putExtra("activeActions", actions.toArray());
+
+        Log.d("ActionExecutorIntentService", "Sending broadcast");
         sendBroadcast(intent);
     }
 
     private List<String> getActiveActions() {
+        Log.d("ActiveActions", "Finding active actions in database");
         List<String> activeActions = new ArrayList<>();
         ParseQuery<ParseObject> query = new ParseQuery<>("Action");
 
         try {
             for (ParseObject parseObject : query.find()) {
                 if (parseObject.getBoolean("active")) {
-                    activeActions.add(parseObject.getString("actionName"));
+                    String activeAction = parseObject.getString("actionName");
+                    Log.d("ActiveActions", "Found:" + activeAction);
+                    activeActions.add(activeAction);
                 }
             }
         } catch (ParseException e) {
+            Log.d("ActiveActions", "ParseException occurred");
             e.printStackTrace();
         }
 
+        Log.d("ActiveActions", "Found " + activeActions.size() + " active actions");
         return activeActions;
     }
 }
