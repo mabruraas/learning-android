91180143558290bbbb57d4cd7331bed92b0c81f2
Espen Gudmundsen <espen.gudmundsen@accenture.com>
2016-01-06 20:01:23 +0100

    CC-1: Created all activities for game.

---
+++ ColorClicker/app/src/main/java/no/bacheloroppgaven/android/colorclicker/lobby/LobbyActivity.java
@@ -0,0 +1,15 @@
+package no.bacheloroppgaven.android.colorclicker.lobby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import no.bacheloroppgaven.android.colorclicker.R;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
