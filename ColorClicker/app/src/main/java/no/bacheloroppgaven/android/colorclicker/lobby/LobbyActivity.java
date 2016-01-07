7a48213fc3cdc9bf2f8397928bf26e12c02a393a
mabruraas <mabruraas@gmail.com>
2016-01-07 01:23:49 +0100

    CC-1: Added fragment for mode selection and a replacement fragment to be activated from the selection.

--- ColorClicker/app/src/main/java/no/bacheloroppgaven/android/colorclicker/lobby/LobbyActivity.java
+++ ColorClicker/app/src/main/java/no/bacheloroppgaven/android/colorclicker/lobby/LobbyActivity.java
@@ -1,7 +1,9 @@
 package no.bacheloroppgaven.android.colorclicker.lobby;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 import no.bacheloroppgaven.android.colorclicker.R;
 
@@ -11,5 +13,11 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+//        ft.add(R.id.activity_lobby_fragment_container, SelectModeFragment.newInstance("", ""));
+        ft.commit();
     }
 }
